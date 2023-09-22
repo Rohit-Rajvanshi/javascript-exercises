498a4c10c1d5b745990ca5f6c314a6fefeabadb1
Rohit-Rajvanshi <ryugarohit123@gmail.com>
2023-09-22 17:01:05 +0530

    We live we lie we love

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...elementToRemove) {
+    const newArray = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      if (elementToRemove.includes(array[i])) {
+        // If the element matches what we want to remove, skip it
+        continue;
+      }
+      newArray.push(array[i]);
+    }
+  
+    return newArray;
+  }
+  
+  const resultArray = removeFromArray([1, 2, 3, 4], 3,2)
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start,stop) {
+    let Sum=0
+    if (!Number.isInteger(start)||!Number.isInteger(stop)){
+        return "ERROR"
+    }
+    if (start<0 || stop<0){
+        return "ERROR"
+    }
+    if(start>stop){
+        let temp=start;
+        start=stop;
+        stop=temp;
 
+    }
+    for(let i=start;i<=stop;i++){
+    Sum+=i;
+    }
+    return Sum;
+    
+    
 };
-
+sumAll(123,1)
 // Do not edit below this line
 module.exports = sumAll;
